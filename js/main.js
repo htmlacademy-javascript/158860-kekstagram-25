c58ac0e8a4307541266a2eb201af2733e750fa1b
mntyred <sunjenec@gmail.com>
2022-02-23 16:40:10 +0300

    доработка домашнего задания 2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-
-function getNumberInt (min, max) {
-  if (min >= 0 && max > 0 && max > min){
-    const number = Math.floor(Math.random() * (max - min + 1 ) + min);
+function getRandomInt(min, max) {
+  if (min >= 0 && max > 0 && max > min) {
+    const number = Math.floor(Math.random() * (max - min + 1) + min);
     return number;
-  } else {
-    return 'Неверный диапазон';
   }
+  return 'Неверный диапазон';
 }
-getNumberInt (5,25);
+getRandomInt(5, 25);
 
-function getCommentLength (comment, maxLength) {
+function checkString(comment, maxLength) {
   if (comment.length <= maxLength) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
-getCommentLength('Тест', 5);
+checkString('Тест', 5);
